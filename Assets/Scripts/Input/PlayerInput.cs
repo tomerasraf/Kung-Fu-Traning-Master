4034f41c75218690b50ff0be1f12078ea9097969
TomerAsraf <tomerasraf2@gmail.com>
2023-02-26 23:56:46 +0200

    Input Manager

---
+++ Assets/Scripts/Input/PlayerInput.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    public void LeftTrigger() 
+    {
+        Debug.Log("Left Trigger Pressed");
+    }
+    public void RightTrigger() 
+    {
+        Debug.Log("Right Trigger Pressed");
+    }
+}
