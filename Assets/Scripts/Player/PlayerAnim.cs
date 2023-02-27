0fe3fb47a1777338524e7f0f217165e00e67f89c
TomerAsraf <tomerasraf2@gmail.com>
2023-02-27 19:57:16 +0200

    trigger update

--- Assets/Scripts/Player/PlayerAnim.cs
+++ Assets/Scripts/Player/PlayerAnim.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 public class PlayerAnim : MonoBehaviour
 {
     [SerializeField]
-    Animator anim;
+    Animator _anim;
     [SerializeField]
-    float _smoothRotation = 5f;
-    [SerializeField]
-    float _smoothTransition = 5f;
+    float _attackDuration = 0.5f;
 
     private bool isResettingMovement;
     private void OnEnable()
@@ -24,58 +22,21 @@ public class PlayerAnim : MonoBehaviour
         PlayerInput.LeftTriggerAction -= LeftTrigger;
     }
 
-    private void Update()
-    {
-        TransitionAnimation();
-        ReturnIdleState();
-    }
-
-    private void TransitionAnimation()
-    {
-        if (anim.GetFloat("Movement") > 0)
-        {
-            // rotate the player smoothly to the left by 90 degrees
-            TurnPlayer(90);
-        }
-        else if (anim.GetFloat("Movement") < 0)
-        {
-            TurnPlayer(-90);
-        }
-        else
-        {
-            TurnPlayer(0);
-        } 
-    }
-
-    private void TurnPlayer(float angle)
-    {
-        Quaternion Turn = Quaternion.Euler(0, angle, 0);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Turn, _smoothRotation * Time.deltaTime);
-    }
-
-    private void ReturnIdleState()
-    {
-        if (anim.GetFloat("Movement") > 0.1f)
-        {
-            anim.SetFloat("Movement", 0, _smoothTransition, Time.deltaTime);
-        }
-        else if (anim.GetFloat("Movement") < -0.1f)
-        {
-            anim.SetFloat("Movement", 0, _smoothTransition, Time.deltaTime);
-        }
-        else
-        {
-            anim.SetFloat("Movement", 0);
-        }
-    }
-
     private void LeftTrigger()
     {
-        anim.SetFloat("Movement", -1);
+        StartCoroutine(Trigger("Left Trigger", "Right Trigger"));
     }
 
     private void RightTrigger()
     {
-        anim.SetFloat("Movement", 1);
+        StartCoroutine(Trigger("Right Trigger", "Left Trigger"));
+    }
+
+    IEnumerator Trigger(string triggerEnable, string triggerDisable)
+    {
+        _anim.SetBool(triggerDisable, false);
+        _anim.SetBool(triggerEnable, true);
+        yield return new WaitForSeconds(_attackDuration);
+        _anim.SetBool(triggerEnable, false);
     }
 }
