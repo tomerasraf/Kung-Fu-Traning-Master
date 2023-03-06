e4be09888da58800439b3533c73d9a8fc222ac39
TomerAsraf <tomerasraf2@gmail.com>
2023-03-06 02:46:21 +0200

    Bomb Collision fix

--- Assets/Scripts/Game Mechanics/FlyingDeadlyObject.cs
+++ Assets/Scripts/Game Mechanics/FlyingDeadlyObject.cs
@@ -26,6 +26,7 @@ public class FlyingDeadlyObject : MonoBehaviour
 
         if (collision.gameObject.CompareTag("Floor"))
         {
+            _flyingObjectRB.AddForce(new Vector3(0, 0, -1 * 2), ForceMode.Impulse);
             StartCoroutine(ExplodeAfterTime(2f));
 
             ScoreAndCombo.instance.IncreaseCombo();
